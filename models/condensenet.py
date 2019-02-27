1566d0ffc111866e5eed795ef9bfb2eb74a74722
Prakhar Bansal <jainvandit15@gmail.com>
2019-02-27 15:21:03 +0530

    removes unwanted commented out code

--- models/condensenet.py
+++ models/condensenet.py
@@ -54,34 +54,6 @@ class _DenseBlock(nn.Sequential):
         for i in range(num_layers):
             layer = _DenseLayer(in_channels , growth_rate, args)
             self.add_module('denselayer_%d' % (i + 1), layer)
-# class _DenseBlock(nn.Module):
-#     def __init__(self, in_channels, growth_rate, args):
-#         super(_DenseBlock, self).__init__()
-
-#         self.layer_1 = _DenseLayer(in_channels , growth_rate, args)
-#         self.layer_2 = _DenseLayer(in_channels + growth_rate, growth_rate, args)
-#         self.layer_3 = _DenseLayer(in_channels + 2*growth_rate, growth_rate, args)
-#         self.layer_4 = _DenseLayer(in_channels + 3*growth_rate , growth_rate, args)
-#         self.layer_5 = _DenseLayer(in_channels + 4*growth_rate , growth_rate, args)
-#         self.layer_6 = _DenseLayer(in_channels + 5*growth_rate , growth_rate, args)
-#         self.layer_7 = _DenseLayer(in_channels + 6*growth_rate , growth_rate, args)
-
-    # def forward(self, x):
-    #     x_1 = self.layer_1(x)
-    #     #x = torch.cat([x,x_1],1)
-    #     x_2 = self.layer_2(x_1)
-    #     #x = torch.cat([x,x_1],1)
-    #     x_3 = self.layer_3(x_2)
-    #     #x = torch.cat([x,x_1],1)
-    #     x_4 = self.layer_4(x_3)
-    #     #x = torch.cat([x,x_1],1)
-    #     x_5 = self.layer_5(x_4)
-    #     #x = torch.cat([x,x_1],1)
-    #     x_6 = self.layer_6(x_5)
-    #     # x = torch.cat([x,x_1],1)
-    #     x_7 = self.layer_7(x_6)
-    #     x = torch.cat([x,x_1,x_2,x_3,x_4,x_5, x_6, x_7],1)
-    #     return x
 
 class CondenseNet(nn.Module):
     def __init__(self, args):
@@ -97,62 +69,10 @@ class CondenseNet(nn.Module):
 
         self.features = nn.Sequential()
 
-####################        Edited by me     ###################################
-
         self.num_features = 10 * self.growth[0]
         self.init_conv = nn.Conv2d(args.c_dim, self.num_features, kernel_size=3,
                                     stride=self.init_stride,padding=1,
                                     bias=False)
-        # self.init_relu = nn.LeakyReLU(0.1,inplace=True)
-        # self.block_0 = _DenseBlock(in_channels=self.num_features,
-        #                             growth_rate=self.growth[0],
-        #                             args=self.args)
-        # self.num_features = 8 * self.num_features + 28 * self.growth[0] 
-        # self.block_1 = _DenseBlock(in_channels=self.num_features,
-        #                             growth_rate=self.growth[1],
-        #                             args=self.args)
-        # self.num_features = 8 * self.num_features + 28 * self.growth[1]
-        # for i in range(len(self.stages)):
-        #     self.add_block(i)
-        # self.dense_layers = [None] * len(self.stages)
-        # for i in range(len(self.stages)):
-        #     self.dense_layers[i] = [None] * self.stages[i]
-        # for i in range(len(self.stages)):
-        #     for j in range(self.stages[i]):
-        #         self.dense_layers[i][j] = _DenseLayer(self.num_features + j * self.growth[i], self.growth[i], args).cuda()
-        #         self.num_features += self.num_features + j * self.growth[i]
-        #     self.num_features += self.stages[i] * self.growth[i]
-                # self.num_features2 = self.num_features2
-        #     # self.dense_block[i] =  _DenseBlock(
-        #     #                     num_layers=self.stages[i],
-        #     #                     in_channels=self.num_features,
-        #     #                     growth_rate=self.growth[i],
-        #     #                     args=self.args)
-        #     self.num_features += self.stages[i] * self.growth[i]
-        #     self.feature_out += self.num_features   #+ self.num_features
-        # self.block_0 = _DenseBlock(num_layers=self.stages[0],
-        #   in_channels=self.num_features,
-        #   growth_rate=self.growth[0],
-        #   args=self.args)
-        # self.num_features += self.stages[0] * self.growth[0] + self.num_features
-
-        # self.block_1 = _DenseBlock(num_layers=self.stages[1],
-        #   in_channels=self.num_features,
-        #   growth_rate=self.growth[1],
-        #   args=self.args)
-        # self.num_features += self.stages[1] * self.growth[1] + self.num_features
-
-        #self.block_2 = _DenseBlock(num_layers=self.stages[2],
-            #in_channels=self.num_features,
-            #growth_rate=self.growth[2],
-            #args=self.args)
-        #self.num_features += self.stages[2] * self.growth[2] + self.num_features
-
-        #self.block_3 = _DenseBlock(num_layers=self.stages[3],
-            #in_channels=self.num_features,
-            #growth_rate=self.growth[3],
-            #args=self.args)
-        #self.num_features += self.stages[3] * self.growth[3] + self.num_features
         self.block1 = _DenseBlock(
             num_layers=self.stages[0],
             in_channels=self.num_features,
@@ -171,10 +91,7 @@ class CondenseNet(nn.Module):
             args=self.args,
         )
         self.num_features += self.num_features
-        # self.num_features += self.stages[1] * self.growth[1]
         self.end_relu  = nn.LeakyReLU(0.1, inplace=True)
-        # self.bottleneck_conv = Conv(self.feature_out, 128, kernel_size=1, padding=0, flag = True)
-        # print (self.num_features)
         self.bottleneck_conv = nn.Conv2d(self.num_features, 256,
                                             kernel_size=1,
                                             stride=self.init_stride,
@@ -192,50 +109,6 @@ class CondenseNet(nn.Module):
                                     stride=1,
                                     padding=0,
                                     bias=False)
-#######################################################################################
-
-        # self.features.add_module('init_conv', nn.Conv2d(3, self.num_features,
-        #                                                 kernel_size=3,
-        #                                                 stride=self.init_stride,
-        #                                                 padding=1,
-        #                                                 bias=False))
-        # self.features.add_module('init_relu', nn.ReLU(inplace=True))
-        # for i in range(len(self.stages)):
-        #     ### Dense-block i
-        #     self.add_block(i)
-        ### Linear layer
-        # self.classifier = nn.Linear(self.num_features, args.num_classes)
-        # self.features.add_module('bottleneck_conv', nn.Conv2d(self.num_features, 256,
-        #                                                 kernel_size=1,
-        #                                                 stride=self.init_stride,
-        #                                                 padding=0,
-        #                                                 bias=False))
-        # for i in range(int(math.log(args.scaling_factor,2))):
-        #     self.features.add_module('deconv_%d' %(i+1), nn.ConvTranspose2d(256, 256,
-        #                                                 kernel_size=2,
-        #                                                 stride=2,
-        #                                                 padding=0,
-        #                                                 bias=False))
-        #     self.features.add_module('Prelu_deconv_%d' %(i+1),
-        #                              nn.PReLU())
-        # self.features.add_module('reconv', nn.Conv2d(256, args.c_dim,
-        #                                                 kernel_size=1,
-        #                                                 stride=1,
-        #                                                 padding=0,
-        #                                                 bias=False))
-        ### initialize
-        # for m in self.modules():
-        #     if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
-        #         n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-        #         m.weight.data.normal_(0, math.sqrt(2. / n))
-        #         if m.bias is not None:
-        #             m.bias.data.zero_()
-            # if isinstance(m, nn.ConvTranspose2d):
-                #c1, c2, h, w = m.weight.data.size()
-                #weight = get_upsample_filter(h)
-                #m.weight.data = weight.view(1, 1, h, w).repeat(c1, c2, 1, 1)
-                #if m.bias is not None:
-                    #m.bias.data.zero_()   
         return
 
     def add_block(self, i):
@@ -246,15 +119,12 @@ class CondenseNet(nn.Module):
             args=self.args,
         )
         self.features.add_module('denseblock_%d' % (i + 1), block)
-        # self.num_features += self.stages[i] * self.growth[i]
 
 
     def forward(self, x, x_,progress=None):
         if progress:
             LearnedGroupConv.global_progress = progress
         x = self.init_conv(x)
-        # x = self.init_relu(x)
-        # x = self.features(x)
         x = self.block1(x)
         x = self.mid_relu(x)
         x = self.mid_conv(x)
--- models/condensenet_converted.py
+++ models/condensenet_converted.py
@@ -68,7 +68,6 @@ class CondenseNet(nn.Module):
         self.features = nn.Sequential()
         ### Initial nChannels should be 3
         self.num_features = 2 * self.growth[0]
-        ########### EDITED by Vandit 30-3 ###############
         self.bottleneck_input = self.num_features
         self.num_features = 2 * self.growth[0]
         ### Dense-block 1 (224x224)
@@ -82,21 +81,9 @@ class CondenseNet(nn.Module):
         for i in range(len(self.stages)):
             for j in range(self.stages[i]):
                 self.dense_layers[i][j] = _DenseLayer(self.num_features + j * self.growth[i], self.growth[i], args).cuda()
-            # self.dense_block[i] =  _DenseBlock(
-            #                     num_layers=self.stages[i],
-            #                     in_channels=self.num_features,
-            #                     growth_rate=self.growth[i],
-            #                     args=self.args)
             self.num_features += self.stages[i] * self.growth[i]
             self.bottleneck_input += self.num_features #+ self.num_features
         self.end_relu  = nn.LeakyReLU(0.1,inplace=True)
-        # self.bottleneck_conv = nn.Conv2d(self.num_features, 256,
-        #                                     kernel_size=1,
-        #                                     stride=self.init_stride,
-        #                                     padding=0,
-        #                                     bias=False)
-        # deconv = [None]*int(math.log(args.scaling_factor,2))
-        # Prelu_deconv = [None]*int(math.log(args.scaling_factor,2))
         self.bottleneck_conv = CondenseConv(self.num_features, 128, kernel_size = 1, groups=self.group_1x1)
         
         self.deconv = nn.ConvTranspose2d(128, 128,
@@ -111,35 +98,7 @@ class CondenseNet(nn.Module):
                                     padding=0,
                                     bias=False)
 
-        ######################### edited by Vandit 30-3 #################
-        ### Dense-block 1 (224x224)
-    #     self.features.add_module('init_conv', nn.Conv2d(3, self.num_features,
-    #                                                     kernel_size=3,
-    #                                                     stride=self.init_stride,
-    #                                                     padding=1,
-    #                                                     bias=False))
-    #     for i in range(len(self.stages)):
-    #         ### Dense-block i
-    #         self.add_block(i)
-    #     ### Linear layer
-    #     # self.classifier = nn.Linear(self.num_features, args.num_classes)
-    #     self.features.add_module('bottleneck_conv', nn.Conv2d(self.num_features, 256,
-    #                                                     kernel_size=1,
-    #                                                     stride=self.init_stride,
-    #                                                     padding=0,
-    #                                                     bias=False))
-    #     for i in range(int(math.log(args.scaling_factor,2))):
-    #         self.features.add_module('deconv', nn.ConvTranspose2d(256, 256,
-    #                                                     kernel_size=2,
-    #                                                     stride=2,
-    #                                                     padding=0,
-    #                                                     bias=False))
-    #     self.features.add_module('reconv', nn.Conv2d(256, args.c_dim,
-    #                                                     kernel_size=1,
-    #                                                     stride=1,
-    #                                                     padding=0,
-    #                                                     bias=False))
-    #     ### initialize
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m,nn.ConvTranspose2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -147,60 +106,20 @@ class CondenseNet(nn.Module):
             elif isinstance(m, nn.ConvTranspose2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
-    #         elif isinstance(m, nn.BatchNorm2d):
-    #             m.weight.data.fill_(1)
-    #             m.bias.data.zero_()
-    #         elif isinstance(m, nn.Linear):
-    #             m.bias.data.zero_()
-
-    # def add_block(self, i):
-    #     ### Check if ith is the last one
-    #     last = (i == len(self.stages) - 1)
-    #     block = _DenseBlock(
-    #         num_layers=self.stages[i],
-    #         in_channels=self.num_features,
-    #         growth_rate=self.growth[i],
-    #         args=self.args,
-    #     )
-    #     self.features.add_module('denseblock_%d' % (i + 1), block)
-    #     self.num_features += self.stages[i] * self.growth[i]
-    #     if not last:
-    #         trans = _Transition(in_channels=self.num_features,
-    #                             args=self.args)
-    #         self.features.add_module('transition_%d' % (i + 1), trans)
-    #     else:
-    #         self.features.add_module('norm_last',
-    #                                  nn.BatchNorm2d(self.num_features))
-    #         self.features.add_module('relu_last',
-    #                                  nn.ReLU(inplace=True))
-    #         self.features.add_module('pool_last',
-    #                                  nn.AvgPool2d(self.pool_size))
 
     def forward(self, x, x_, progress=None):
-        # features = self.features(x)
-        # out = features.view(features.size(0), -1)
-        # out = self.classifier(out)
         x = self.init_conv(x)
         x = self.init_relu(x)
-        # p = x
         h = []
         h.append(x)
         for i in range(len(self.stages)):
             for j in range(self.stages[i]):
                 x = self.dense_layers[i][j](x)
                 h.append(x)
-                # x = torch.cat([x,p],1)
-                # for k in range(j+1):
-                #   x = torch.add(x,list[k])
-                # list.append(x)
-            # p = x
-        # for i in h:
-        #     x = torch.cat([x, i], 1)
         x = self.end_relu(x)
         x = self.bottleneck_conv(x)
         for i in range(int(math.log(self.args.scaling_factor,2))):
             x = self.deconv(x)
             x = self.relu_deconv(x)
         x = self.reconv(x)
-        # x_ = bicubic(x_,self.args)
         return torch.add(x, x_)
